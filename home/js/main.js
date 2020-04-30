ff4f1c717594bd9208001b8342903022766cda40
Jael Johnson <jajo6032@colorado.edu>
2020-04-30 17:21:17 -0600

    Upload

---
+++ home/js/main.js
@@ -0,0 +1,124 @@
+// Code found online at Codepen.io 
+//I used the anime.js code as a skeleton and worked off it and modified it for this project. 
+
+AOS.init()
+// $(function(){
+
+//  $('#anibutt').click(function(){
+//    animate('#anihead', 'bounce');
+//    animate('#anibody', 'rubberBand');
+//    return false;
+// });
+//  function animate(element, animation){
+//    $(element).addClass('animated '+animation);
+//    var wait = setTimeout(function(){
+//      $(element).removeClass('animated '+animation);
+//    }, 1000);
+//  }
+// });
+
+AOS.init({
+  // Global settings:
+  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: 'aos-init', // class applied after initialization
+  animatedClassName: 'aos-animate', // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+  
+
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 400, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+});
+
+
+class TextScramble {
+  constructor(el) {
+    this.el = el
+    this.chars = '!<>-_\\/[]{}—=+*^?#________'
+    this.update = this.update.bind(this)
+  }
+  setText(newText) {
+    const oldText = this.el.innerText
+    const length = Math.max(oldText.length, newText.length)
+    const promise = new Promise((resolve) => this.resolve = resolve)
+    this.queue = []
+    for (let i = 0; i < length; i++) {
+      const from = oldText[i] || ''
+      const to = newText[i] || ''
+      const start = Math.floor(Math.random() * 40)
+      const end = start + Math.floor(Math.random() * 40)
+      this.queue.push({ from, to, start, end })
+    }
+    cancelAnimationFrame(this.frameRequest)
+    this.frame = 0
+    this.update()
+    return promise
+  }
+  update() {
+    let output = ''
+    let complete = 0
+    for (let i = 0, n = this.queue.length; i < n; i++) {
+      let { from, to, start, end, char } = this.queue[i]
+      if (this.frame >= end) {
+        complete++
+        output += to
+      } else if (this.frame >= start) {
+        if (!char || Math.random() < 0.28) {
+          char = this.randomChar()
+          this.queue[i].char = char
+        }
+        output += `<span class="dud">${char}</span>`
+      } else {
+        output += from
+      }
+    }
+    this.el.innerHTML = output
+    if (complete === this.queue.length) {
+      this.resolve()
+    } else {
+      this.frameRequest = requestAnimationFrame(this.update)
+      this.frame++
+    }
+  }
+  randomChar() {
+    return this.chars[Math.floor(Math.random() * this.chars.length)]
+  }
+}
+
+
+const phrases = [
+  'Hey,',
+  'Did you know',
+  'Coronavirus is highly contagious',
+  'Want to learn more about it and how you can still have fun?',
+  'keep',
+  'on',
+  'reading',
+  'Lets begin!'
+]
+
+const el = document.querySelector('.text')
+const fx = new TextScramble(el)
+
+let counter = 0
+const next = () => {
+  fx.setText(phrases[counter]).then(() => {
+    setTimeout(next, 2000)
+  })
+  counter = (counter + 1) % phrases.length
+}
+
+next()
+
+
+
---
+++ tips/js/main.js
@@ -0,0 +1,40 @@
+//Idea found in a youtube video and on anime.js website
+//I used the anime.js code as a skeleton and worked off it and modified it for this project. 
+
+let tl = anime.timeline({
+    easing: "easeOutExpo",
+    duration: 3500,
+    loop:true
+  })
+
+tl.add({
+  targets: ".container .column",
+  backgroundColor: "rgb(250, 250, 155)",
+  width: "90%",
+  delay: anime.stagger(500)
+})
+
+tl.add({
+  targets: ".container .column",
+  backgroundColor: "rgb(255, 210, 77)",
+  width: "100%",
+})
+
+anime({
+  targets: '.main-title',
+  rotate: {
+    value: 360,
+    duration: 1800,
+    easing: 'easeInOutQuart'
+  },
+  scale: {
+    value: 2.5,
+    duration: 1600,
+    delay: 2500,
+    endDelay: 2000,
+    // easing: 'easeInOutQuart'
+  },
+  delay: 500, // All properties except 'scale' inherit 250ms delay
+  loop: true
+})
+
